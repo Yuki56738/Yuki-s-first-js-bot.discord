ebaa516a6f94b664cff51c3034e95b52263ec094
Yuki <enginner@risaton.net>
2022-02-25 15:21:34 +0900

    use config.json instead of dotenv

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { Client, Intents } = require('discord.js')
-require('dotenv').config;
-const token = process.env.DISCORD_TOKEN;
+
+const { token } = require('./config.json')
+console.log(token)
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
 
