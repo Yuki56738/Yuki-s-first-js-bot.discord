0528c1a539fdba965dcbcfaaa2283a871635c901
Yuki <enginner@risaton.net>
2022-02-25 14:58:25 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const { Client, Intents } = require('discord.js')
+require('dotenv').config;
+const token = process.env.DISCORD_TOKEN;
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
+
+client.once('ready', () => {
+  console.log('Bot is ready.')
+  console.log(`Logged in as: ${client.user.tag}`)
+})
+
+client.on('interactionCreate', async interaction => {
+  console.log('interactionCreate hit.')
+  if (!interaction.isCommand()) return;
+  
+  const {commandName} = interaction;
+  
+  console.log(`commandName: ${commandName}`)
+  
+  if (commandName === 'yuki') {
+    console.log('command yuki hit.')
+    await interaction.reply('Pong!')
+    console.log('sent "Pong!"')
+  } else if (commandName === 'yuki2') {
+    console.log('command yuki2 hit.')
+    await interaction.reply(`Server name: ${interaction.guild.name}\n Total members: ${interaction.guild.memberCount}`)
+    console.log('sent.')
+  }
+
+})
+
+client.login(token)
